0bb7080b4c476a932aec0a752293d3bc889a5363
rakib <rakibsarowar@gmail.com>
2023-02-11 11:23:57 +0600

    2nd commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// Adding color style in H2 ------------------------------------------------
+
+const headTwo = document.querySelectorAll('h2');
+for (const head of headTwo){
+    // console.log(head);
+    head.style.color = 'lightblue';
+}
+
+// Adding color sytle in section ----------------------------------
+const backpackSection = document.getElementById('backpack');
+backpackSection.style.backgroundColor = 'tomato';
+
+// Targeting 
